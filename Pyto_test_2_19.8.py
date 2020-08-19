a0f35fa3dbf954196403cd30c2debf482d5c9c1a
Martin Köhler <junker-joerg@users.noreply.github.com>
2020-08-19 17:29:21 +0200

    neu generiert

--- Pyto_test_2_19.8.py
+++ Pyto_test_2_19.8.py
@@ -1,3 +1,16 @@
 # Created with Pyto
 
+
+import os
+
+act_dir = os.getcwd()
+
 print("Hello World! MK")
+print(act_dir)
+
+
+file = "test.txt"
+with open(file, "w") as file_handle:
+    for i in range(10):
+        file_handle.write("Hannover")        
+
