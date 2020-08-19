f85836b42f22e396fa83e7912f6fc0f9cc9a9b9b
Martin Köhler <junker-joerg@users.noreply.github.com>
2020-08-19 18:03:36 +0200

    aus dem Zug

--- Pyto_test_2_19.8.py
+++ Pyto_test_2_19.8.py
@@ -12,5 +12,15 @@ print(act_dir)
 file = "test.txt"
 with open(file, "w") as file_handle:
     for i in range(10):
-        file_handle.write("Hannover")        
+        file_handle.write("Hannove\n")
+        
+# file_handle.close()
 
+file = "pyto_test.txt"
+
+with open(file, "r") as file_handle:
+    s = file_handle.readlines()
+    
+    
+print(s)
+        
